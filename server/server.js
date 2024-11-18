88e35c9c5ead242625e5368e195bc0ace5a43a95
jakobferguson <jakob.b.ferguson@gmail.com>
2024-11-18 16:04:00 -0600

    Refactored file structure to use client and server folders.
    Server can now be run using "node server.js".
    -Added require for dotenv
    
    Added .env file for environment variables.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-//TO DO
-const app = require('./app');
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
